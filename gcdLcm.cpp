dc65aa549d8090577f871286ee74658552227568
jihyeon <nsa06120@naver.com>
2021-01-03 12:09:29 +0900

    알고리즘랩스- GCD LCM

---
+++ gcdLcm.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+	int r;
+	while (b != 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+int main() {
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	if (b > a) {
+		int tmp;
+		tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	printf("%d\n", gcd(a, b));
+	printf("%d\n", lcm(a, b));
+
+	return 0;
+}
