986720e4535628e437a90e677b62a3491064e0a9
jihyeon <20181039@sungshin.ac.kr>
2021-02-20 21:40:38 +0900

    이진 탐색 (반복문)

---
+++ 개념/binary_search(while).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target, int start, int end) {
+    while (start <= end) {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == target) 
+			return mid;
+        else if (arr[mid] > target) 
+			end = mid - 1;
+
+        else start = mid + 1; 
+    }
+    return -1;
+}
+
+int main(void) {
+    int n, target;
+    
+    cin >> n >> target;
+
+	vector<int> arr(N);
+
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+
+    int result = binarySearch(arr, target, 0, n - 1);
+    
+    if (result == -1) 
+        cout << "원소가 존재하지 않습니다." << '\n';
+    else 
+        cout << result + 1 << '\n';
+}
